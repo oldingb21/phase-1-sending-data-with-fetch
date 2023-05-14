d74aa663ba3c16dcd41a728c6ffdf32fc48de9f7
Brenden Olding <oldingb21@gmail.com>
2023-05-14 01:31:52 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const submitData = (userName, userEmail) => {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json",
+          },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail})
+    })
+    .then((res) => res.json())
+    .then((userData) => {
+        const userId = document.createElement('p');
+        const body = document.querySelector('body')
+        userId.textContent = userData.id;
+        body.appendChild(userId);
+    })
+    .catch((error) => {
+        const errorOnDOM = document.createElement('p');
+        const body = document.querySelector('body');
+        errorOnDOM.textContent = error.message;
+        body.appendChild(errorOnDOM);
+    })
+}
